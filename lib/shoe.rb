12aa29f505445a41e449217de05cd848fa15887b
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-19 06:01:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,21 @@
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+require 'pry'
 
+class Shoe
+  
+  #attr_accessor :brand
+  attr_reader :brand
+  
+  @@BRANDS = []
+  
   def initialize(brand)
     @brand = brand
+    #@@BRANDS << self
   end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+  
+  def brand=(brand)
+    @brand = brand
+    #self.uniq_brands
+    @@BRANDS << brand
   end
-
+  
 end
